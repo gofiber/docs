930c66667417968336c7858e5f056b425143df2c
René <rene@gofiber.io>
2024-04-21 13:56:19 +0200

    [v3-Maintenance]-Consolidate-and-Document-Core-Changes-in-v3

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -296,6 +296,7 @@ const config = {
             prism: {
                 theme: lightCodeTheme,
                 darkTheme: darkCodeTheme,
+                additionalLanguages: ['json'],
             },
         }),
 };
